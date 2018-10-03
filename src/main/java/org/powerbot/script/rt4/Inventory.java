c4cd0475b240383122559cb4b2895dbcc9d86529
Ash Greh <emailforansh@gmail.com>
2018-10-04 01:53:44 +0530

    Fixes drag
    The previous drag was not compatible with fixed mode.

--- src/main/java/org/powerbot/script/rt4/Inventory.java
+++ src/main/java/org/powerbot/script/rt4/Inventory.java
@@ -10,8 +10,6 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.awt.image.ImageObserver.HEIGHT;
-import static java.awt.image.ImageObserver.WIDTH;
 
 /**
  * Inventory
@@ -23,7 +21,7 @@ public class Inventory extends ItemQuery<Item> {
 
 	@Override
 	protected List<Item> get() {
-		final List<Item> items = new ArrayList<Item>(Constants.INVENTORY_SIZE);
+		final List<Item> items = new ArrayList<>(Constants.INVENTORY_SIZE);
 		final Component comp = component();
 		if (comp.componentCount() > 0) {
 			for (final Component c : comp.components()) {
@@ -111,7 +109,7 @@ public class Inventory extends ItemQuery<Item> {
 	/**
 	 * Drops specified item via regular or shift dropping.
 	 *
-	 * @param i     The item to drop
+	 * @param i	 The item to drop
 	 * @param shift Shift dropping, if true the method will verify it is enabled and fall back to regular if not
 	 * @return Success
 	 */
@@ -151,24 +149,56 @@ public class Inventory extends ItemQuery<Item> {
 			return true;
 		}
 	}
-	
+
+	/**
+	 * Converts an index to it's positional representation.
+	 * Example, item at index 0 is returned as (0,0) or 7 as (1,2).
+	 * @param index 0-(Constants.INVENTORY_SIZE-1), index of inventory
+	 * @return position representation of the index
+	 * @throws IndexOutOfBoundsException if index is below 0 or above (Constants.INVENTORY_SIZE-1)
+	 */
+	public Point indexPosition(int index) {
+		if(index < 0 || index > Constants.INVENTORY_SIZE - 1){
+			throw new IndexOutOfBoundsException();
+		}
+		int xFactor = index%4;
+		int yFactor = Math.floorDiv(index, 4);
+		return new Point(xFactor, yFactor);
+	}
+
+	/**
+	 * Gives center for a position (x,y).
+	 * Example, item at index 0 is referenced by (0,0) or 7 by (1,2).
+	 *
+	 * @param point the position to calculate for.
+	 * @return centre point of the specified point
+	 * @throws IndexOutOfBoundsException if x is not between 0 to 3 or y is not between 0 to 6
+	 */
+	public Point centerPoint(Point point) {
+		if(point.x < 0 || point.x > 3 || point.y < 0 || point.y > 6){
+			throw new IndexOutOfBoundsException();
+		}
+		//DIFFERENCE BETWEEN CENTER POINTS
+		int xFactor = ((point.x)*Constants.INVENTORY_ITEM_X_DIFFERENCE)+18;
+		int yFactor = ((point.y)*Constants.INVENTORY_ITEM_Y_DIFFERENCE)+16;
+		//INVENTORY WIDGET POSITION
+		Point inventoryBase = ctx.widgets.component(Constants.INVENTORY_WIDGET, 0).screenPoint();
+		return new Point(inventoryBase.x+xFactor, inventoryBase.y+yFactor);
+	}
+
 	/**
 	 * Finds the centerPoint of the inventory's index
 	 * @param index 0-(Constants.INVENTORY_SIZE-1), index of inventory
 	 * @return centerPoint of the index param
 	 * @throws IndexOutOfBoundsException if index is below 0 or above (Constants.INVENTORY_SIZE-1)
 	 */
-	public Point indexCenterPoint(int index){
+	public Point centerPoint(int index){
 		if(index < 0 || index > Constants.INVENTORY_SIZE - 1){
 			throw new IndexOutOfBoundsException();
 		}
-		
-		final Point base = component().screenPoint();
-		final int x = base.x - 3 + (index % 4) * WIDTH;
-		final int y = base.y - 2 + (index / 4) * HEIGHT;
-		return new Point(x + WIDTH / 2, y + HEIGHT / 2);
+		return centerPoint(indexPosition(index));
 	}
-	
+
 	/**
 	 * Finds the boundingRectangle of the desired index, not every area within the rectangle will click the item
 	 * @param index 0-(Constants.INVENTORY_SIZE-1), index of inventory
@@ -176,20 +206,14 @@ public class Inventory extends ItemQuery<Item> {
 	 * @throws IndexOutOfBoundsException if index is below 0 or above (Constants.INVENTORY_SIZE-1)
 	 */
 	public Rectangle boundingRect(int index){
-		if(index < 0 || index > Constants.INVENTORY_SIZE-1){
-			throw new IndexOutOfBoundsException();
-		}
-		
-		final Point base = component().screenPoint();
-		final int x = base.x - 3 + (index % 4) * WIDTH;
-		final int y = base.y - 2 + (index / 4) * HEIGHT;
-		
-		return new Rectangle(x, y, WIDTH, HEIGHT);
+		final int xFactor = Constants.ITEM_WIDTH/2, yFactor = Constants.ITEM_HEIGHT/2;
+		Point centerPoint = centerPoint(index);
+		return new Rectangle(centerPoint.x-xFactor, centerPoint.y-yFactor, Constants.ITEM_WIDTH, Constants.ITEM_HEIGHT);
 	}
-	
+
 	/**
 	 * Drags the given item to the given index
-	 * 
+	 *
 	 * @param item Item to be dragged
 	 * @param index Index to drag the item to
 	 * @return True if the item is at the index or the drag was successful, false otherwise
@@ -199,30 +223,30 @@ public class Inventory extends ItemQuery<Item> {
 		if(!item.valid()){
 			return false;
 		}
-		
+
 		if(item.inventoryIndex() == index){
 			return true;
 		}
-		
+
 		if(index < 0 || index > Constants.INVENTORY_SIZE-1){
 			throw new IndexOutOfBoundsException();
 		}
-		
+
 		if(!ctx.input.move(item.nextPoint())){
 			return false;
 		}
-		
+
 		final Rectangle r = boundingRect(index);
 		final int xOff = r.width / 8, yOff = r.height / 8;
 		final Rectangle objectRectangle = new Rectangle(r.x + r.width / 2 - xOff, r.y + r.height / 2 - yOff, r.width / 4, r.height / 4);
-		
+
 		return ctx.input.drag(Calculations.nextPoint(r, objectRectangle), true);
 	}
 
 	public boolean shiftDroppingEnabled() {
 		return ctx.varpbits.varpbit(1055, 17, 0x1) == 1;
 	}
-	
+
 	public boolean isFull() {
 		return ctx.inventory.select().size() >= Constants.INVENTORY_SIZE;
 	}
