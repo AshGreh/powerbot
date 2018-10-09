24123cef4a6d19f367703f0732e5ce234f2fff0d
Ash Greh <emailforansh@gmail.com>
2018-10-09 13:24:18 +0530

    Change to itemAt & row column methods

--- src/main/java/org/powerbot/script/rt4/Inventory.java
+++ src/main/java/org/powerbot/script/rt4/Inventory.java
@@ -10,13 +10,11 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.awt.image.ImageObserver.HEIGHT;
-import static java.awt.image.ImageObserver.WIDTH;
-
 /**
  * Inventory
  */
 public class Inventory extends ItemQuery<Item> {
+	
 	public Inventory(final ClientContext ctx) {
 		super(ctx);
 	}
@@ -79,8 +77,30 @@ public class Inventory extends ItemQuery<Item> {
 		return items;
 	}
 
+	/**
+	 * Gives you the item at a certain index.
+	 *
+	 * @param index range is 0-27.
+	 * @return the item at the index. If the item index is greater than INVENTORY_SIZE, returns a nil item.
+	 */
 	public Item itemAt(final int index) {
-		return index >= 0 && index < Constants.INVENTORY_SIZE ? items()[index] : nil();
+		final Component comp = component(), itemComponent;
+		if(index > -1 && index < Constants.INVENTORY_SIZE && comp.componentCount() > index && (itemComponent = comp.component(index)).id() > -1 && itemComponent.id() != 6512 && comp.itemStackSize() > -1 ) {
+			return new Item(ctx, itemComponent, itemComponent.id(), itemComponent.itemStackSize());
+		} else {
+			return nil();
+		}
+	}
+
+	/**
+	 * Gives you an item at a certain position.
+	 *
+	 * @param row the row position for the inventory item (0-7)
+	 * @param column the column position for the inventory item. (0-3)
+	 * @return the item at the index. If the item index is greater than INVENTORY_SIZE, returns a nil item.
+	 */
+	public Item itemAt(int row, int column) {
+		return itemAt(inventoryIndex(row, column));
 	}
 
 	public int selectionType() {
@@ -153,65 +173,84 @@ public class Inventory extends ItemQuery<Item> {
 	}
 	
 	/**
+	 * Gives the row of a given index.
+	 * Does not throw an exception if index is out of range.
+	 *
+	 * @param index ranges from 0 - (INVENTORY_SIZE-1)
+	 * @return the column position for the index (0-4)
+	 */
+	public int inventoryColumn(int index) {
+		return index % 4;
+	}
+
+	/**
+	 * Gives the row of a given index.
+	 * Does not throw an exception if index is out of range.
+	 *
+	 * @param index ranges from 0 - (INVENTORY_SIZE-1)
+	 * @return the row position for the index (0-7)
+	 */
+	public int inventoryRow(int index) {
+		return Math.floorDiv(index, 4);
+	}
+
+	/**
+	 * Gives the index for a given row and column.
+	 * Does not throw an exception if row/column are out of range.
+	 *
+	 * @param row the row position for the inventory item (0-7)
+	 * @param column the column position for the inventory item. (0-3)
+	 * @return the index for the given.
+	 */
+	public int inventoryIndex(int row, int column) {
+		return row*4 + column;
+	}
+
+	/**
 	 * Converts an index to it's positional representation.
-	 * Example, item at index 0 is returned as (0,0) or 7 as (1,2).
+	 * Example, item at index 0 is returned as (0,0) or 6 as (1,2).
+	 * This exists only for people who want a single atomic call to get inventory column and row.
+	 * Doesn't throw an excception if out of bound.
+	 *
 	 * @param index 0-(Constants.INVENTORY_SIZE-1), index of inventory
 	 * @return position representation of the index
-	 * @throws IndexOutOfBoundsException if index is below 0 or above (Constants.INVENTORY_SIZE-1)
 	 */
 	public Point indexPosition(int index) {
-		if(index < 0 || index > Constants.INVENTORY_SIZE - 1){
-			throw new IndexOutOfBoundsException();
-		}
-		int xFactor = index % 4;
-		int yFactor = Math.floorDiv(index, 4);
-		return new Point(xFactor, yFactor);
+		return new Point(inventoryColumn(index), inventoryRow(index));
 	}
-	
+
 	/**
-	 * Gives center for a position (x,y).
-	 * Example, item at index 0 is referenced by (0,0) or 7 by (1,2).
+	 * Gives center for a position (row, column).
+	 * Example, item at index 0 is referenced by (0,0) or 6 by (1,2).
 	 *
-	 * @param point the position to calculate for.
+	 * @param row the row position for the inventory item (0-7)
+	 * @param column the column position for the inventory item. (0-3)
 	 * @return centre point of the specified point
 	 * @throws IndexOutOfBoundsException if x is not between 0 to 3 or y is not between 0 to 6
 	 */
-	public Point centerPoint(Point point) {
-		if(point.x < 0 || point.x > 3 || point.y < 0 || point.y > 6){
+	public Point centerPoint(int row, int column) {
+		if(column < 0 || column > 3 || row < 0 || row > 6){
 			throw new IndexOutOfBoundsException();
 		}
 		//DIFFERENCE BETWEEN CENTER POINTS
-		int xFactor = ((point.x)*Constants.INVENTORY_ITEM_X_DIFFERENCE)+18;
-		int yFactor = ((point.y)*Constants.INVENTORY_ITEM_Y_DIFFERENCE)+16;
+		int xFactor = (column*Constants.INVENTORY_ITEM_X_DIFFERENCE)+18;
+		int yFactor = (row*Constants.INVENTORY_ITEM_Y_DIFFERENCE)+16;
 		//INVENTORY WIDGET POSITION
 		Point inventoryBase = ctx.widgets.component(Constants.INVENTORY_WIDGET, 0).screenPoint();
 		return new Point(inventoryBase.x+xFactor, inventoryBase.y+yFactor);
 	}
-	
-	/**
-	 * Gives center for a position (x,y).
-	 * Example, item at index 0 is referenced by (0,0) or 7 by (1,2).
-	 *
-	 * @param x the x position for the inventory item. (0-3)
-	 * @param y the y position for the inventory item (0-6)
-	 * @return centre point of the specified point
-	 * @throws IndexOutOfBoundsException if x is not between 0 to 3 or y is not between 0 to 6
-	 */
-	public Point centerPoint(int x, int y) {
-		return centerPoint(new Point(x, y));
-	}
-	
+
 	/**
 	 * Finds the center point of the inventory's index
 	 * @param index 0-(Constants.INVENTORY_SIZE-1), index of inventory
 	 * @return centerPoint of the index param
 	 * @throws IndexOutOfBoundsException if index is below 0 or above (Constants.INVENTORY_SIZE-1)
 	 */
-	public Point indexCenterPoint(int index){
+	public Point centerPoint(int index){
 		if(index < 0 || index > Constants.INVENTORY_SIZE - 1){
 			throw new IndexOutOfBoundsException();
 		}
-		return centerPoint(indexPosition(index));
+		return centerPoint(inventoryRow(index), inventoryColumn(index));
 	}
 	
 	/**
